ff65b22c8a5b394ae5324512fbb42fcaed298041
iosmanthus <myosmanthustree@gmail.com>
2022-08-22 15:27:43 +0800

    add overlay
    Signed-off-by: iosmanthus <myosmanthustree@gmail.com>

--- flake.nix
+++ flake.nix
@@ -11,6 +11,9 @@
       };
     in
     {
+      overlay = self: super: {
+        feishu = self.packages.${system}.feishu;
+      };
       packages.${system}.feishu = (pkgs.callPackage ./default.nix { });
       legacyPackages.${system}.feishu = (pkgs.callPackage ./default.nix { });
     };
